751fbf688369a7005c08a7e336d623af248474b7
Richard do Carmo Lopes <32149536+RichardCLopes@users.noreply.github.com>
2022-09-05 16:04:13 -0300

    updates de funções

--- agenda.js
+++ agenda.js
@@ -1,34 +1,60 @@
+var lista = [];
 
-    var lista =[];
+function inserir() {
+  var id = window.prompt("ID: ");
+  var nome = window.prompt("Nome: ");
+  var tel = window.prompt("Telefone: ");
+  var registro = new Contato(id, nome, tel);
+  lista.push(registro);
+}
 
-    function inserir() {
-        var id = window.prompt("ID: ");
-        var nome = window.prompt("Nome: ");
-        var tel = window.prompt("Telefone: ");
-
-        var Contato = new Contato(id, nome, tel);
-        lista.push(Contato);
+function remover() {
+  var id = window.prompt("ID do contato a ser deletado: ");
+  for (let i = 0; i <= lista.length; i++) {
+    if (id == lista[i].getId()) {
+      lista.splice(i, 1);
     }
+  }
+}
 
-    function remover() {
-        var id = window.prompt("ID do contato a ser deletado: ");
-        for(i,j in lista){
-            if(j[0]==id){
-                delete lista[i];
-            }
-        }
+function buscar() {
+  var id = window.prompt("ID do contato a ser buscado: ");
+  for (let i = 0; i <= lista.length; i++) {
+    if (id == lista[i].getId()) {
+      document.getElementById("cont").innerText = lista[i].mostraRegistro();
     }
+  }
+}
 
-    function buscar() {
-        var id = window.prompt("ID do contato a ser buscado: ");
-    }
+function listar() {
+  document.getElementById("cont").innerText = "";
+  for (let i = 0; i <= lista.length; i++) {
+    var escrita = document.getElementById("cont").innerText;
+    document.getElementById("cont").innerText =
+      escrita + "\n" + lista[i].mostraRegistro();
+  }
+}
 
-    function listar() {
-        var escrita;
-        for(i in lista){
-            escrita += i.id  + i.nome + i.telefone;
-        }
+class Contato {
+  constructor(id, nome, telefone) {
+    this.id = id;
+    this.nome = nome;
+    this.telefone = telefone;
+  }
 
-        document.getElementById("cont").innerText(escrita);
-    }
+  getId() {
+    return this.id;
+  }
 
+  mostraRegistro() {
+    return (
+      "ID: " +
+      this.id +
+      "\nNome: " +
+      this.nome +
+      "\nNumero: " +
+      this.telefone +
+      "\n--------------------------------\n"
+    );
+  }
+}
