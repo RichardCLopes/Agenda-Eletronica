b7f5a4acbbdc75caef9a3ccdda50c9195378efc2
Richard do Carmo Lopes <32149536+RichardCLopes@users.noreply.github.com>
2022-09-11 03:05:31 -0300

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,20 +1,32 @@
+var ageletronica = new Agenda([]);
+function inserir() {
+  ageletronica.inserir();
+}
+function remover() {
+  ageletronica.remover();
+}
+function buscar() {
+  ageletronica.buscar();
+}
+function listar() {
+  ageletronica.listar();
+}
+
 //CLASSE CONTATO------------------------------------
-class Contato {
-  constructor(id, nome, telefone) {
-    this.id = id;
-    this.nome = nome;
-    this.telefone = telefone;
-  }
+function Contato(id, nome, telefone) {
+  this.id = id;
+  this.nome = nome;
+  this.telefone = telefone;
 
-  getId() {
+  this.getid = function () {
     return this.id;
-  }
+  };
 
-  getNome() {
+  this.getnome = function () {
     return this.nome;
-  }
+  };
 
-  mostraRegistro() {
+  this.mostraregistro = function () {
     return (
       "ID: " +
       this.id +
@@ -24,51 +36,52 @@ class Contato {
       this.telefone +
       "\n--------------------------------\n"
     );
-  }
+  };
 }
 
 //CLASSE AGENDA ----------------------------
+function Agenda(lista) {
+  var _lista = lista; //variavel privada
 
-var lista = [];
+  this.inserir = function () {
+    var id = window.prompt("ID: ");
+    var nome = window.prompt("Nome: ");
+    var tel = window.prompt("Telefone: ");
+    var registro = new Contato(id, nome, tel);
+    lista.push(registro);
+  };
 
-function inserir() {
-  var id = window.prompt("ID: ");
-  var nome = window.prompt("Nome: ");
-  var tel = window.prompt("Telefone: ");
-  var registro = new Contato(id, nome, tel);
-  lista.push(registro);
-}
-
-function remover() {
-  var id = window.prompt("ID do contato a ser deletado: ");
-  for (let i = 0; i <= lista.length; i++) {
-    if (id == lista[i].getId()) {
-      lista.splice(i, 1);
+  this.remover = function () {
+    var id = window.prompt("ID do contato a ser deletado: ");
+    for (let i = 0; i <= lista.length; i++) {
+      if (id == lista[i].getid()) {
+        lista.splice(i, 1);
+        document.getElementById("cont").innerText = "REMOVIDO";
+      }
     }
-  }
-  document.getElementById("cont").innerText = "REMOVIDO";
-}
+  };
 
-function buscar() {
-  var id = window.prompt("ID do contato a ser buscado: ");
-  for (let i = 0; i <= lista.length; i++) {
-    if (id == lista[i].getId()) {
-      document.getElementById("cont").innerText = lista[i].mostraRegistro();
+  this.buscar = function () {
+    var id = window.prompt("ID do contato a ser buscado: ");
+    for (let i = 0; i <= lista.length; i++) {
+      if (id == lista[i].getid()) {
+        document.getElementById("cont").innerText = lista[i].mostraregistro();
+      }
     }
-  }
-}
+  };
 
-function listar() {
-  lista.sort(function (a, b) {
-    if (a.getNome() < b.getNome()) return -1;
-    if (a.getNome() > b.getNome()) return 1;
-    return 0;
-  });
+  this.listar = function () {
+    lista.sort(function (a, b) {
+      if (a.getnome() < b.getnome()) return -1;
+      if (a.getnome() > b.getnome()) return 1;
+      return 0;
+    });
 
-  document.getElementById("cont").innerText = "";
-  for (let i = 0; i <= lista.length; i++) {
-    var escrita = document.getElementById("cont").innerText;
-    document.getElementById("cont").innerText =
-      escrita + "\n" + lista[i].mostraRegistro();
-  }
+    document.getElementById("cont").innerText = "";
+    for (let i = 0; i <= lista.length; i++) {
+      var escrita = document.getElementById("cont").innerText;
+      document.getElementById("cont").innerText =
+        escrita + "\n" + lista[i].mostraregistro();
+    }
+  };
 }
